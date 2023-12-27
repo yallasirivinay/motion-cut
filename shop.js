f32d6ade87d76294c47020fe202e9452c6122a20
YALLASIRI VINAY <109100736+yallasirivinay@users.noreply.github.com>
2023-12-27 20:48:45 +0530

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,7 @@
+// JavaScript for adding items to the cart
+document.getElementById('addToCartBtn').addEventListener('click', function() {
+    // Here, you can add functionality to add the product to the cart
+    // For demonstration, you can display an alert message
+    alert('Product added to cart!');
+  });
+  
